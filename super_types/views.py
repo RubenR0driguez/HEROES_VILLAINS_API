d56901ce3e4bd0f9307111eb3b75ae459072720d
ruben rodriguez <rotap45r@gmail.com>
2022-04-18 16:28:26 -0400

    created supers app

--- super_types/views.py
+++ super_types/views.py
@@ -1,9 +1,12 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import SuperType
+from .serializers import SuperTypeserializer
 
 @api_view(['GET'])
 def super_type_list(request):
+    super_type = SuperType.objects.all()
+    serializer = SuperTypeserializer(super_type, many=True)
     
-    
-    
- return Response('ok')
+
+    return Response(serializer.data)
--- supers/admin.py
+++ supers/admin.py
@@ -1,6 +1,3 @@
 from django.contrib import admin
-from .models import SuperType
-
 
 # Register your models here.
-admin.site.register(SuperType)
